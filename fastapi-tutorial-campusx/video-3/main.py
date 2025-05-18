fb06b0c423e8f4ded52a22ddd1a78ce5bf18e058
ritesh-raushan <rausahnritesh@gmail.com>
2025-05-18 12:46:30 +0530

    Post and Delete Endpoints

--- fastapi-tutorial-campusx/video-3/main.py
+++ fastapi-tutorial-campusx/video-3/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from pydantic import BaseModel
+from enum import Enum
 import json
 
 app = FastAPI()
@@ -7,6 +9,10 @@ def load_data():
     with open("patients.json", "r") as f:
         data = json.load(f)
     return data
+
+def save_data(data):
+    with open("patients.json", "w") as f:
+        json.dump(data, f, indent=4)
 
 @app.get("/")
 def hello():
@@ -38,3 +44,48 @@ def get_patient(patient_id: str = Path(..., description="The ID of the patient",
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail="Patient not found")
+
+class genderEnum(str, Enum):
+    male = "male"
+    female = "female"
+
+class verdictEnum(str, Enum):
+    obese = "obese"
+    overweight = "overweight"
+    underweight = "underweight"
+    normal = "normal"
+class PatientModel(BaseModel):
+    name: str
+    city: str
+    age: int
+    gender: genderEnum
+    height: float
+    weight: float
+    bmi: float
+    verdict: verdictEnum
+
+@app.post("/add")
+def add_patient(patient: PatientModel):
+    data = load_data()
+    if data:
+        last_id = max(int(k[1:]) for k in data.keys())
+    else:
+        last_id = 0
+    new_id = f"P{last_id + 1:03}"
+
+    data[new_id] = patient.model_dump()
+
+    save_data(data)
+
+    return {"message": "Patient added successfully", "patient_id": new_id}
+
+@app.delete("/delete/{patient_id}")
+def delete_patient(patient_id: str):
+    data = load_data()
+    if patient_id not in data:
+        raise HTTPException(status_code = 404, detail = "Patient not Fount")
+    
+    del data[patient_id]
+    save_data(data)
+
+    return{"message": f"Patient {patient_id} deleted successfully"}
